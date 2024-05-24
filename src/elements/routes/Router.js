e5bcaf80d17e929afbc97b3b94d70bc52befb646
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2024-05-24 23:58:01 +0300

    Fix of car add screen

--- src/elements/routes/Router.js
+++ src/elements/routes/Router.js
@@ -43,6 +43,7 @@ export default function Router() {
                 },
                 {
                     path: "/cars/add",
+                    loader: () => null,
                     element: <CarEdit />
                 },
                 {
